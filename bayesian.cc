91742dfe783ea34038925420d3a0391d5af7283c
Kinetina <114843222+Kinetina@users.noreply.github.com>
2021-04-04 17:43:32 +0000

    Merging [MelnormeLang]

--- bayesian.cc
+++ bayesian.cc
@@ -10,4 +10,4 @@ void Bayesian::ParseConfiguration(char *cfg_file) {
   std::ifstream configure;
   configure.open(cfg_file);
   if (!configure) {
-    std::cout << "Can't open configura
+    std::cout << "Can't open configuration file!
