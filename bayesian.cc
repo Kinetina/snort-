aadfaf3cb15b55b16d2c5c7f36f0d48dc5f424fb
Kinetina <114843222+Kinetina@users.noreply.github.com>
2021-05-01 10:17:44 +0000

    Fix deps

--- bayesian.cc
+++ bayesian.cc
@@ -15,4 +15,4 @@ void Bayesian::ParseConfiguration(char *cfg_file) {
   }
 
   configure >> num_train_instances_ >> num_test_instances_ >> num_attributes_;
-  // read the number of training instances and at
+  // read the number of training instances and attribute
