e8bb7f5bbf8f7114f7cccffcc228b203493b6daa
Kinetina <114843222+Kinetina@users.noreply.github.com>
2021-07-11 21:47:18 +0000

    Update call-to-action.html

--- bayesian.cc
+++ bayesian.cc
@@ -22,4 +22,5 @@ void Bayesian::ParseConfiguration(char *cfg_file) {
   for (int i = 0; i < num_attributes_; ++i) configure >> is_discrete_[i];
   //  read the information about continuous or not
 
-  num_class_for_each_attribute_.resize(num_attributes_ +
+  num_class_for_each_attribute_.resize(num_attributes_ + 1);
+  /
