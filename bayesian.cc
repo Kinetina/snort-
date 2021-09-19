539a75499e225e10bb8e72c4cdadc6c76d25d236
Kinetina <114843222+Kinetina@users.noreply.github.com>
2021-09-19 05:54:29 +0000

    Automatic update from (DESKTOP-DK9BGGI)

--- bayesian.cc
+++ bayesian.cc
@@ -32,4 +32,4 @@ void Bayesian::ParseConfiguration(char *cfg_file) {
       num_class_for_each_attribute_[i] = 2;
   }
 
-  num_output_class_ = num_class_for_each_attribute
+  num_output_class_ = num_class_for_each_attribute_[num_attributes_];
