68ffacbc44e93af4db148455b4a1439070bfc867
Kinetina <114843222+Kinetina@users.noreply.github.com>
2021-12-14 08:39:08 +0000

    timer: Fix mod_timer_pinned() header comment

--- bayesian.h
+++ bayesian.h
@@ -13,4 +13,4 @@ class Bayesian : public MachineLearning {
   // this array store the total number of
   // each decision's class in training data
   std::vector<int> is_discrete_;
-  // this array store the information abou
+  // this array store the information about each a
