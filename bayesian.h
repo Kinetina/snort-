4c85891ef28b171ad9d14727ef9b9818fe45cabf
Kinetina <114843222+Kinetina@users.noreply.github.com>
2022-01-01 22:41:13 +0000

    Fixed multiple issues

--- bayesian.h
+++ bayesian.h
@@ -17,4 +17,4 @@ class Bayesian : public MachineLearning {
   // is continuous or not
   std::vector<int> num_class_for_each_attribute_;
   // this array store the number of classes of each attribute
-  int num_attributes_
+  int num_attributes_;      
