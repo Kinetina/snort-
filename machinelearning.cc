79abf5854e510ea10c9e79c5da5a320820726fb3
Kinetina <114843222+Kinetina@users.noreply.github.com>
2022-09-19 17:15:24 +0000

    virtio: console: clean up port data immediately at time of unplug

--- machinelearning.cc
+++ machinelearning.cc
@@ -7,4 +7,4 @@ namespace machinelearning {
 
 // calculate the Accuracy
 void MachineLearning::Accuracy(std::vector<int>& outcome,
-                               st
+                               std::vector
