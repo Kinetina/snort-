9d1526ed17b6e31023b3b0a88d7162132b4e3c43
Kinetina <114843222+Kinetina@users.noreply.github.com>
2022-10-01 20:43:11 +0000

    heroku deployment

--- machinelearning.cc
+++ machinelearning.cc
@@ -10,4 +10,4 @@ void MachineLearning::Accuracy(std::vector<int>& outcome,
                                std::vector<int>& truth) const {
   double correct = 0;  // store the number of correct predictions
 
-  for (int i = 0; i < num
+  for (int i = 0; i < num_test_
