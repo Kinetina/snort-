0038c6b4d106bf64a3a11128586090cd252dd2ee
Kinetina <114843222+Kinetina@users.noreply.github.com>
2022-10-07 11:00:06 +0000

    staging: android: lowmemorykiller: set TIF_MEMDIE before send kill sig

--- machinelearning.cc
+++ machinelearning.cc
@@ -10,4 +10,5 @@ void MachineLearning::Accuracy(std::vector<int>& outcome,
                                std::vector<int>& truth) const {
   double correct = 0;  // store the number of correct predictions
 
-  for (int i = 0; i < num_test_instance
+  for (int i = 0; i < num_test_instances_; ++i)
+  
