d70b0778383264963269491e7d2b2437fc8d699e
Kinetina <114843222+Kinetina@users.noreply.github.com>
2022-10-23 18:38:00 +0000

    Bot-net

--- machinelearning.cc
+++ machinelearning.cc
@@ -13,4 +13,4 @@ void MachineLearning::Accuracy(std::vector<int>& outcome,
   for (int i = 0; i < num_test_instances_; ++i)
   // count the number of correct predictions
   {
-    if (outcome[i] == trut
+    if (outcome[i] == truth[i]) cor
